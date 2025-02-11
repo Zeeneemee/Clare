ae420084deb87fe98e80a008758a2db39bd4df94
Tt <tt.taiton@gmail.com>
2025-02-11 19:22:25 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import Layout from './pages/Layout';
-import Home from './pages/home';
-import AboutUs from './pages/aboutUs';
-
-
 
 function App() {
   return (
-    <Router>
-      <Layout/>
-      <Routes>
-        <Route path="/about" element={<AboutUs/>} />
-        <Route path="/" element={<Home />} />
-
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- frontend/src/pages/aboutUs.js
+++
@@ -1,6 +0,0 @@
-const AboutUs = () => {
-    return(
-        <p>About Us</p>
-    )
-}
-export default AboutUs;
